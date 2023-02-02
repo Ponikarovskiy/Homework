3a4a006057adc18730e15a7ba94eea665bbf5060
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-02 21:30:24 +0300

    Решена задача № 6

--- Program.cs
+++ Program.cs
@@ -34,38 +34,68 @@
 // // 44 5 78 -> 78
 // // 22 3 9 -> 22
 
-System.Console.WriteLine();
-System.Console.WriteLine("*Задача № 4. Поиск наибольшего значения из трех чисел*");
-System.Console.WriteLine();
+// System.Console.WriteLine();
+// System.Console.WriteLine("*Задача № 4. Поиск наибольшего значения из трех чисел*");
+// System.Console.WriteLine();
 
-System.Console.WriteLine("Введите первое число");
-if (!int.TryParse(Console.ReadLine(), out int number1))
-{
-    Console.WriteLine("Введено не целое число");
-    return;
-}
-System.Console.WriteLine("Введите второе число");
-if (!int.TryParse(Console.ReadLine(), out int number2))
-{
-    Console.WriteLine("Введено не целое число");
-    return;
-}
-System.Console.WriteLine("Введите третье число");
-if (!int.TryParse(Console.ReadLine(), out int number3))
-{
-    Console.WriteLine("Введено не целое число");
-    return;
-}
+// System.Console.WriteLine("Введите первое число");
+// if (!int.TryParse(Console.ReadLine(), out int number1))
+// {
+//     Console.WriteLine("Введено не целое число");
+//     return;
+// }
+// System.Console.WriteLine("Введите второе число");
+// if (!int.TryParse(Console.ReadLine(), out int number2))
+// {
+//     Console.WriteLine("Введено не целое число");
+//     return;
+// }
+// System.Console.WriteLine("Введите третье число");
+// if (!int.TryParse(Console.ReadLine(), out int number3))
+// {
+//     Console.WriteLine("Введено не целое число");
+//     return;
+// }
 
-int max = number1;
+// int max = number1;
 
-if (number2 > max)
-{
-    max = number2;
-}
-if (number3 > max)
-{
-    max = number3;
-}
+// if (number2 > max)
+// {
+//     max = number2;
+// }
+// if (number3 > max)
+// {
+//     max = number3;
+// }
 
-System.Console.WriteLine($"max = {max}");
+// System.Console.WriteLine(max);
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("*Задача № 6. Проверка числа на четность*");
+// System.Console.WriteLine();
+
+// System.Console.WriteLine("Введите число");
+// if (!int.TryParse(Console.ReadLine(), out int number))
+// {
+//     Console.WriteLine("Введено не целое число");
+//     return;
+// }
+
+// int result = number % 2;
+
+// if(result == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
